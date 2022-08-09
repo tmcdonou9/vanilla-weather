018ed20deb09423745e6dfaeb328f507d2af2499
tmcdonou9 <110191044+tmcdonou9@users.noreply.github.com>
2022-08-08 20:40:52 -0400

    Added search engine
    Followed along with SheCodes Plus Week 7 Weather App rebuilding example

--- src/app.js
+++ src/app.js
@@ -42,8 +42,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "29a4ad7a3a9232d56b27c341061a6051";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?id=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "29a4ad7a3a9232d56b27c341061a6051";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?id=${city}&appid=${apiKey}&units=metric`;
+  search(cityInputElement.value);
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+}
+
+search("New York");
+
+let form = document.querySelector("search-form");
+form.addEventListener("submit", handleSubmit);
